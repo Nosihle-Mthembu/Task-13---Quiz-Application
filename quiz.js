5f7637db0d1ec1da8897191c03ee0da1a0f9c7d6
Nosihle Mthembu <bongekam16@gmail.com>
2024-10-24 10:20:46 +0200

    feat:readline interface

---
+++ quiz.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+// Set up readline interface for user input
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+
+
+// Start the quiz
+startQuiz();
